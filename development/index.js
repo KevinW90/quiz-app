9578d0b5876209024a3f772b3e6382c4e805b337
KevinW90 <kevinjwilliams76@gmail.com>
2020-07-23 01:51:59 -0400

    finished quiz logic

--- development/index.js
+++ development/index.js
@@ -6,7 +6,7 @@ const STORE = [
       'Nowhere',
       'Everywhere',
       'Somewhere',
-      'why do you need to know?'
+      'Why do you need to know?'
     ],
     correctAnswer: 2
   },
@@ -26,7 +26,7 @@ const STORE = [
       'Nothing',
       'Everything',
       'Something',
-      'why do you need to know?'
+      'Why do you need to know?'
     ],
     correctAnswer: 3
   },
@@ -36,7 +36,7 @@ const STORE = [
       'Nothing',
       'Everything',
       'Something',
-      'why do you need to know?'
+      'Why do you need to know?'
     ],
     correctAnswer: 1
   },
@@ -46,11 +46,146 @@ const STORE = [
       'Nothing',
       'Everything',
       'Something',
-      'why do you need to know?'
+      'Why do you need to know?'
     ],
     correctAnswer: 2
   }
 ];
+
+//variables for quiz info
+let score;
+let curQ;
+let totalQuestions;
+
+
+const clickHandler = (e) => {
+  e.preventDefault();
+  let btnText = e.target.textContent.toLowerCase() || e.target.value.toLowerCase();
+  console.log(btnText);
+  switch(btnText){
+    case 'start':
+      startQuiz();
+      break;
+    case 'final answer':
+      checkAnswer();
+      break;
+    case 'next':
+      //update question count
+      curQ++;
+      console.log(curQ)
+      //display next question
+      displayQuestion();
+      break;
+    case 'retake':
+      startQuiz();
+      break;
+    default:
+      alert("That doesn't do anything");
+  }
+}
+
+const displayQuestion = () => {
+  //check for existing question
+  if (curQ < totalQuestions) {
+    //get current question details
+    const questionText = STORE[curQ].question;
+    const possibleAnswers = STORE[curQ].answers;
+
+    //quiz details
+    $('#currentQuestion').text(curQ + 1);
+    $('#totalQuestions').text(totalQuestions);
+    $('#score').text(score);
+
+    //populate form fields
+    $('#questionArea').text(questionText);
+    possibleAnswers.forEach((el, ndx) => {
+      $(`label[for=${ndx}]`).text(el);
+    })
+
+    //take off previous selection
+    $('form > div').removeClass('selected correct incorrect');
+
+    //change to 'final answer'
+    $('input[type="submit"]').val('Final Answer');
+  } else {
+    console.log('hi')
+    displayResults();
+  }
+}
+
+const displayResults = () => {
+  //hide quiz
+  $('.question-display').hide();
+  //show results page
+  $('.results-page').show()
+  //calculations
+  let percentage = Math.round((score / totalQuestions) * 100, 2);
+  $('#questionAmount').text(totalQuestions);
+  $('#correctAnswers').text(score);
+  $('#incorrectAnswers').text(totalQuestions - score);
+  $('#percentage').text(percentage + '%');
+
+  percentage > 70 ? $('#pass-fail').text('PASSED') : $('#pass-fail').text('FAILED')
+}
+
+const startQuiz = () => {
+  //set quiz control variables
+  score = 0;
+  curQ = 0;
+  totalQuestions = STORE.length;
+  //display a question
+  displayQuestion();
+  //hide start page
+  $('.start-page').hide();
+  //show quiz
+  $('.question-display').show();
+}
+
+const checkAnswer = () => {
+  let userChoice = $($('input[name="choice"]:checked')[0]);
+  console.log(userChoice);
+  if (userChoice.val() == STORE[curQ].correctAnswer) {
+    score++;
+    $('#score').text(score);
+    //highlight green
+    userChoice.parent().addClass('correct');
+  } else {
+    //user choice red
+    userChoice.parent().addClass('incorrect');
+    //correct answer green
+    $(`input[id=${STORE[curQ].correctAnswer}]`).parent().addClass('correct');
+    console.log('wrong')
+  }
+
+  //change button text
+  $('input[type="submit"]').val('Next');
+}
+
+$(document).ready(function(){
+  //click listener for every button
+  $('[class="btn"]').on('click', function(event){
+    clickHandler(event);
+  })
+
+  //form div listener to check radio button
+  $('form > div').on('click', function(event){
+    //set all radio buttons to false
+    $('input[type="radio"]').prop("checked", false);
+    //find closest radio button, and check it
+    $(this).find('input').prop("checked", true);
+    //remove any selected classes
+    $('form > div').removeClass('selected');
+    //add white border to div
+    $(this).addClass('selected');
+  })
+
+  //hide question display area
+  $('.question-display').hide();
+  //hide results page
+  $('.results-page').hide();
+})
+
+
 
 //the page loads with the start screen
 //to start the quiz, the user clicks the 'start quiz' button
